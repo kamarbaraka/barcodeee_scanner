519b3a7103986b80068f11b27fd2c8d79f438665
Kb_hacks <kamar254baraka@gmail.com>
2023-06-07 17:50:30 +0300

    fixed

--- database_api/DatabaseApi.py
+++ database_api/DatabaseApi.py
@@ -5,21 +5,23 @@ import sys
 class DatabaseApi:
 
     try:
-        file = shelve.open('database')
-        database = {}
+        database = shelve.open('database')
+        #database = {}
     except FileExistsError:
         print("database exists")
 
     def parse(self, barcode):
         self.database[barcode] = dict(count=0, type_of_material='plastic')
         self.database['sales'] = 0
-        self.file.close()
+        self.database.close()
 
     @staticmethod
     def fetch(barcode):
         database = shelve.open('database', writeback=True)
+        #print(database)
         database[str(barcode)]['count'] += 1
         barcode_data = database.get(str(barcode))
+        #print(barcode_data)
         return barcode_data
 
 
@@ -38,6 +40,7 @@ if __name__ == '__main__':
             print(db.fetch(inpt))
         except KeyError:
             print('scan again')
+            print()
             continue
     sys.exit(25)
 
